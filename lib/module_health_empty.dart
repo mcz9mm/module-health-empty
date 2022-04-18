a90282ed32a7b4f6a98945011aa40b2c4f30fd9a
mcz9mm <k.m.swim411@gmail.com>
2022-04-18 10:17:19 +0900

    create package module_health for empty & add test code

---
+++ lib/module_health_empty.dart
@@ -0,0 +1,43 @@
+library module_health_empty;
+
+class HealthService {
+  HealthService._privateConstructor();
+
+  static final HealthService _instance = HealthService._privateConstructor();
+
+  static HealthService get instance => _instance;
+
+  Future<List<HealthDataPoint>> getHealthData({
+    /// DateTime Format: DateTime(2021, 11, 09, 0, 0, 0)
+    required DateTime startDate,
+
+    /// DateTime Format: DateTime(2021, 11, 09, 23, 59, 59)
+    required DateTime endDate,
+    List<HealthDataType> types = const [HealthDataType.STEPS],
+  }) async {
+    return [];
+  }
+
+  Future<bool> requestAuthorization(
+      {List<HealthDataType> types = const [HealthDataType.STEPS]}) async {
+    return false;
+  }
+
+  Future<void> revokePermission() async {
+    return;
+  }
+}
+
+enum HealthDataType { STEPS }
+
+// lib_core側で参照するのに必要最低限の実装にする
+class HealthDataPoint {
+  num _value;
+  DateTime _dateFrom;
+  HealthDataPoint(this._value, this._dateFrom);
+
+  num get value => _value;
+
+  /// The start of the time interval
+  DateTime get dateFrom => _dateFrom;
+}
---
+++ test/module_health_empty_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:module_health_empty/module_health_empty.dart';
+
+void main() {
+  test('getHealthData is empty', () async {
+    const period = 30;
+    final currentDate = DateTime.now();
+    final startDate = currentDate.subtract(const Duration(days: period));
+
+    final healthData = await HealthService.instance.getHealthData(
+      startDate:
+          DateTime(startDate.year, startDate.month, startDate.day, 0, 0, 0),
+      endDate: DateTime(
+          currentDate.year, currentDate.month, currentDate.day, 23, 59, 59),
+    );
+    expect(healthData, []);
+  });
+
+  test('requestAuthorization is denied', () async {
+    final isAuthorized = await HealthService.instance
+        .requestAuthorization(types: [HealthDataType.STEPS]);
+    expect(isAuthorized, false);
+  });
+}
